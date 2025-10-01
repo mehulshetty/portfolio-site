11de4028d48579637a25e91d7d0ea77a59ae7f88
Mehul Shetty <mehul@Mehuls-MacBook-Pro.local>
2025-10-01 16:09:07 -0700

    updated footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
   return (
     <footer className="site-footer">
-      <p>Mehul Shetty &copy; {new Date().getFullYear()}. All rights reserved.</p>
+      <p>Mehul Shetty &copy; {new Date().getFullYear()}. All rights reserved. Made using Vite and hosted on GitHub Pages.</p>
     </footer>
   )
 }
